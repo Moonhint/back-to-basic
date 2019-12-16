28599be75883ce614ebd401fb0f915c9f37fbbb1
Antoni <antoni@bukalapak.com>
2019-12-16 16:29:56 +0700

    learn async

---
+++ function/zasync.js
@@ -0,0 +1,22 @@
+
+
+let promiseFunction = () => {
+  return new Promise((resolve, reject)=>{
+    resolve("something");
+  });
+}
+
+let main = async () => {
+  let result = await promiseFunction();
+  console.log(result, "main");
+  return result;
+}
+
+let mainNonAsync = () => {
+  let result = main();
+  console.log(result, "mainNonAsync");
+  return result;
+} 
+
+// main();
+mainNonAsync();
